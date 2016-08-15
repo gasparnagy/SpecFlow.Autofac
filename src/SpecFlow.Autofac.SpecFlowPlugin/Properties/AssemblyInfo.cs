6ddd33b68af38f587ec135bc17538eca035d1120
Gaspar Nagy <gaspar.nagy@gmail.com>
2016-08-15 15:54:02 +0200

    Added NuGet package creation

--- src/SpecFlow.Autofac.SpecFlowPlugin/Properties/AssemblyInfo.cs
+++ src/SpecFlow.Autofac.SpecFlowPlugin/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Autofac.SpecFlowPlugin")]
+[assembly: AssemblyTitle("SpecFlow.Autofac (SpecFlow plugin)")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Autofac.SpecFlowPlugin")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCompany("Spec Solutions")]
+[assembly: AssemblyProduct("SpecFlow.Autofac")]
+[assembly: AssemblyCopyright("Copyright © 2016 Gaspar Nagy, Spec Solutions")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,3 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0-localdev")]
