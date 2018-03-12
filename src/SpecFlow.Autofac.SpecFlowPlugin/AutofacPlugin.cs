46f2c54fec6b592ac0e87d43e814feb249901e86
einsteine89 <einsteine89@yahoo.com>
2018-03-12 14:45:42 +0200

    Fix for https://github.com/gasparnagy/SpecFlow.Autofac/issues/11. Allow the container to resolve ScenarioContext, FeatureContext, TestThreadContext, IObjectContainer. (#15)

--- src/SpecFlow.Autofac.SpecFlowPlugin/AutofacPlugin.cs
+++ src/SpecFlow.Autofac.SpecFlowPlugin/AutofacPlugin.cs
@@ -10,6 +10,10 @@ using TechTalk.SpecFlow.Plugins;
 
 namespace SpecFlow.Autofac
 {
+    using BoDi;
+
+    using TechTalk.SpecFlow;
+
     public class AutofacPlugin : IRuntimePlugin
     {
         public void Initialize(RuntimePluginEvents runtimePluginEvents, RuntimePluginParameters runtimePluginParameters)
@@ -35,10 +39,51 @@ namespace SpecFlow.Autofac
                     var containerBuilderFinder = args.ObjectContainer.Resolve<IContainerBuilderFinder>();
                     var createScenarioContainerBuilder = containerBuilderFinder.GetCreateScenarioContainerBuilder();
                     var containerBuilder = createScenarioContainerBuilder();
+                    RegisterSpecflowDependecies(args.ObjectContainer, containerBuilder);
                     var container = containerBuilder.Build();
                     return container.BeginLifetimeScope();
                 });
             };
+        }
+
+        /// <summary>
+        ///     Fix for https://github.com/gasparnagy/SpecFlow.Autofac/issues/11 Cannot resolve ScenarioInfo
+        ///     Extracted from
+        ///     https://github.com/techtalk/SpecFlow/blob/master/TechTalk.SpecFlow/Infrastructure/ITestObjectResolver.cs
+        ///     The test objects might be dependent on particular SpecFlow infrastructure, therefore the implemented
+        ///     resolution logic should support resolving the following objects (from the provided SpecFlow container):
+        ///     <see cref="ScenarioContext" />, <see cref="FeatureContext" />, <see cref="TestThreadContext" /> and
+        ///     <see cref="IObjectContainer" /> (to be able to resolve any other SpecFlow infrastucture). So basically
+        ///     the resolution of these classes has to be forwarded to the original container.
+        /// </summary>
+        /// <param name="objectContainer">SpecFlow DI container.</param>
+        /// <param name="containerBuilder">Autofac ContainerBuilder.</param>
+        private void RegisterSpecflowDependecies(
+            IObjectContainer objectContainer,
+            global::Autofac.ContainerBuilder containerBuilder)
+        {
+            containerBuilder.Register(ctx => objectContainer).As<IObjectContainer>();
+            containerBuilder.Register(
+                ctx =>
+                {
+                    var specflowContainer = ctx.Resolve<IObjectContainer>();
+                    var scenarioContext = specflowContainer.Resolve<ScenarioContext>();
+                    return scenarioContext;
+                }).As<ScenarioContext>();
+            containerBuilder.Register(
+                ctx =>
+                {
+                    var specflowContainer = ctx.Resolve<IObjectContainer>();
+                    var scenarioContext = specflowContainer.Resolve<FeatureContext>();
+                    return scenarioContext;
+                }).As<FeatureContext>();
+            containerBuilder.Register(
+                ctx =>
+                {
+                    var specflowContainer = ctx.Resolve<IObjectContainer>();
+                    var scenarioContext = specflowContainer.Resolve<TestThreadContext>();
+                    return scenarioContext;
+                }).As<TestThreadContext>();
         }
     }
 }
